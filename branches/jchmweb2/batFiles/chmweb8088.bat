deeb1a8e3bfc6809c618c14fa3a3e4b4e353cc28
forpdfsending@gmail.com <forpdfsending@gmail.com@ebdc000b-f91e-0410-8042-b1b5aa1f422b>
2011-04-07 08:25:47 +0000

    Bat files are updated.

--- branches/jchmweb2/batFiles/chmweb8088.bat
+++ branches/jchmweb2/batFiles/chmweb8088.bat
@@ -1,4 +1,4 @@
 set JCHMLIB=D:/bin/jchmlib
 set CLASSPATH=%CLASSPATH%;%JCHMLIB%/bin/jchmlib.jar
-start java -Djchmweb.template=%JCHMLIB%/bin org.jchmlib.net.ChmWeb 8088 %1
+start java -Djchmweb.template=%JCHMLIB%/bin com.google.code.jchmweb.jchmweb2.net.ChmWeb 8088 %1
 start firefox http://localhost:8888/@index.html
--- branches/jchmweb2/batFiles/chmweb8888.bat
+++ branches/jchmweb2/batFiles/chmweb8888.bat
@@ -1,2 +1,2 @@
-start java -classpath .;jchmweb2.jar -Djchmweb.template=%JCHMLIB%/bin org.jchmlib.net.ChmWeb 8888 %1
+start java -classpath .;jchmweb2.jar -Djchmweb.template=%JCHMLIB%/bin com.google.code.jchmweb.jchmweb2.net.ChmWeb 8888 %1
 start firefox http://localhost:8888/@index.html
